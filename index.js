4e9c69d8730c808a39f629af34bffbfc6f376fac
creinis <115181527+creinis@users.noreply.github.com>
2024-07-03 10:18:51 +0200

    fix: stylesheet for vercel structure

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+app.use('/public', express.static('public'));
+
 
 // Basic route
 app.get('/', (req, res) => {
